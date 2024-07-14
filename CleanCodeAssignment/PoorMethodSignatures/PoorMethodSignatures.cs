81dfada5f48f95cac5957a093a9006daa7f337a2
netch-dev <daijeln361@gmail.com>
2024-07-14 05:42:53 -0700

    PoorMethodSignatures - Removed the bool flag as they as usually a code smell. Extracted the code into two methods that are short and intention revealing

--- CleanCodeAssignment/PoorMethodSignatures/PoorMethodSignatures.cs
+++ CleanCodeAssignment/PoorMethodSignatures/PoorMethodSignatures.cs
@@ -1,62 +1,48 @@
-﻿using System;
-using System.Linq;
+﻿namespace CleanCode.PoorMethodSignatures {
+	public class PoorMethodSignatures {
+		public void Run() {
+			UserService userService = new UserService();
 
-namespace CleanCode.PoorMethodSignatures
-{
-    public class PoorMethodSignatures
-    {
-        public void Run()
-        {
-            var userService = new UserService();
+			User user = userService.Login("username", "password");
+			User anotherUser = userService.GetUser("username");
+		}
+	}
 
-            var user = userService.GetUser("username", "password", true);
-            var anotherUser = userService.GetUser("username", null, false);
-        }
-    }
+	public class UserService {
+		private UserDbContext _dbContext = new UserDbContext();
 
-    public class UserService
-    {
-        private UserDbContext _dbContext = new UserDbContext();
+		public User GetUser(string username) {
+			// Check if there is a user with the given username
+			// If yes, return it, otherwise return null
+			User? user = _dbContext.Users.SingleOrDefault(u => u.Username == username);
+			return user;
+		}
 
-        public User GetUser(string username, string password, bool login)
-        {
-            if (login)
-            {
-                // Check if there is a user with the given username and password in db
-                // If yes, set the last login date 
-                // and then return the user. 
-                var user = _dbContext.Users.SingleOrDefault(u => u.Username == username && u.Password == password);
-                if (user != null)
-                    user.LastLogin = DateTime.Now;
-                return user;
-            }
-            else
-            {
-                // Check if there is a user with the given username
-                // If yes, return it, otherwise return null
-                var user = _dbContext.Users.SingleOrDefault(u => u.Username == username);
-                return user;
-            }
-        }
-    }
+		public User Login(string username, string password) {
+			// Check if there is a user with the given username and password in db
+			// If yes, set the last login date 
+			// and then return the user. 
 
-    public class UserDbContext : DbContext
-    {
-        public IQueryable<User> Users { get; set; }
-    }
+			User? user = _dbContext.Users.SingleOrDefault(u => u.Username == username && u.Password == password);
+			if (user != null)
+				user.LastLogin = DateTime.Now;
+			return user;
+		}
+	}
 
-    public class DbSet<T>
-    {
-    }
+	public class UserDbContext : DbContext {
+		public IQueryable<User> Users { get; set; }
+	}
 
-    public class DbContext
-    {
-    }
+	public class DbSet<T> {
+	}
 
-    public class User
-    {
-        public string Username { get; set; }
-        public string Password { get; set; }
-        public DateTime LastLogin { get; set; }
-    }
+	public class DbContext {
+	}
+
+	public class User {
+		public string Username { get; set; }
+		public string Password { get; set; }
+		public DateTime LastLogin { get; set; }
+	}
 }
